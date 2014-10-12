fc715794162107ddb767d89f6420fd03954e15e0
Benjamin A Ochoa <ben@law451.com>
2014-10-12 05:06:01 -0500

    replaced static services w lbServices, update for contactInfos broken

--- client/nameApp.main.js
+++ client/nameApp.main.js
@@ -1,15 +1,19 @@
 angular.module('nameApp', ['ngRoute', 'lbServices'])
 .controller('mainCtrl', function($scope, AddressBookData, Entry) {
 	
-// Get the addressbook data from our service. Maybe I'll add a loopback server underneath?
+// Get the entries data from our service. 
 
-	$scope.addressBook = AddressBookData;
+	$scope.entries = Entry.find({
+		filter: {
+			include: 'contactInfos'	
+		}
+		});
 
 // Blank form for initializing the address form and resetting it after an add/edit
 
 	var blankForm = {
 		"name": "",
-		"contact": [{ "type": "phone", "address": ""}]
+		"contactInfos": [{ "type": "phone", "value": ""}]
 	};
 	
 	$scope.clearForm = function () {
@@ -20,21 +24,24 @@ angular.module('nameApp', ['ngRoute', 'lbServices'])
 	$scope.$on('$viewContentLoaded', $scope.clearForm());
 
 	$scope.addEntry = function () {
-			$scope.addressBook.push($scope.form);
+			Entry.create($scope.form);
+			$scope.entries.push($scope.form);
 			$scope.clearForm();
 	};
 
 // updateEntry actually updates the entry in the addressbook and resets the form
 
 	$scope.updateEntry = function () {
-		var i = $scope.addressBook.indexOf($scope.edit);
-		$scope.addressBook[i] = $scope.form;
+		Entry.upsert($scope.form);
+		var i = $scope.entries.indexOf($scope.edit);
+		$scope.entries[i] = $scope.form;
 		$scope.clearForm();
 	};
 
 	$scope.removeEntry = function (entry) {
-		var i = $scope.addressBook.indexOf(entry);
-		$scope.addressBook.splice(i, 1);
+		Entry.deleteById(entry);
+		var i = $scope.entries.indexOf(entry);
+		$scope.entries.splice(i, 1);
 	};
 
 // editEntry sets up the form for editing an entry but does not save any changes to the addressbook
@@ -46,20 +53,13 @@ angular.module('nameApp', ['ngRoute', 'lbServices'])
 
 // Address as used in the functions below refers to the contact methods (phone, email) of an entry
 
-	$scope.addAddress = function () {
-		$scope.form.contact.push(angular.copy(blankForm.contact[0]));
+	$scope.addContactValue= function () {
+		$scope.form.contactInfos.push(angular.copy(blankForm.contactInfos[0]));
 	};
 
-	$scope.removeAddress = function (contact) {
-		var i = $scope.form.contact.indexOf(contact);
-		$scope.form.contact.splice(i, 1);
+	$scope.removeContactValue = function (contact) {
+		var i = $scope.form.contactInfos.indexOf(contact);
+		$scope.form.contactInfos.splice(i, 1);
 	};
 
-// Functions for lbServices Entry
-
-	$scope.entries = Entry.find({
-		filter: {
-			include: 'contactInfos'	
-		}
-		});
 });
