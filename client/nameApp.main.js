f0109241f66405e4dd6bd78509ea8d52a37c2fce
Ben Ochoa <ben@law451.com>
2014-10-20 18:22:29 -0500

    updated comments, changed all API calls to use promises

--- client/nameApp.main.js
+++ client/nameApp.main.js
@@ -1,13 +1,28 @@
 angular.module('nameApp', ['ngRoute', 'lbServices'])
 .controller('mainCtrl', function($scope, AddressBookData, Entry, ContactInfo) {
-	
+
+ /*****************
+ * Initialization *
+ *****************/
+
 // Get the entries data from our service. 
 
-	$scope.entries = Entry.find({
-		filter: {
-			include: 'contactInfos'	
-		}
-		});
+	$scope.getEntries = function () {
+		Entry
+			.find({
+				filter: {
+					include: 'contactInfos'	
+					}
+				})
+			.$promise
+			.then(function (entries) {
+					$scope.entries = entries;
+					$scope.clearForm();
+				}, function (error) {
+					alert(error);
+				});
+	}
+
 
 // Blank form for initializing the address form and resetting it after an add/edit
 
@@ -18,51 +33,90 @@ angular.module('nameApp', ['ngRoute', 'lbServices'])
 	
 	$scope.clearForm = function () {
 		$scope.form = angular.copy(blankForm);
-		$scope.edit = null; // edit holds the original copy of the entry being edited
+		$scope.edit = false; 
+		$scope.form.removedContactInfos = [];
 	};
 
-	$scope.$on('$viewContentLoaded', $scope.clearForm());
+// Load blank form once page is loaded
+
+	$scope.$on('$viewContentLoaded', $scope.getEntries() );
+
+ /*************
+ * Create New *
+ *************/
+
+// Add New Entry
 
 	$scope.addEntry = function () {
-			Entry.create($scope.form);
-			$scope.entries.push($scope.form);
-			$scope.clearForm();
+		Entry
+			.create($scope.form)
+			.$promise
+			.then(function(newEntry) {
+				$scope.entries.push(newEntry);
+				$scope.clearForm();
+			});
 	};
 
-// updateEntry actually updates the entry in the addressbook and resets the form
-
-	$scope.updateEntry = function () {
-		Entry.upsert($scope.form);
-//		ContactInfo.upsert($scope.form.contactInfos[0]);
-		for (i = 0; i < $scope.form.contactInfos.length; i++) {
-			ContactInfo.upsert($scope.form.contactInfos[i]);
-		}
-		var i = $scope.entries.indexOf($scope.edit);
-		$scope.entries[i] = $scope.form;
-		$scope.clearForm();
-	};
-
-	$scope.removeEntry = function (entry) {
-		Entry.deleteById(entry);
-		var i = $scope.entries.indexOf(entry);
-		$scope.entries.splice(i, 1);
-	};
-
-// editEntry sets up the form for editing an entry but does not save any changes to the addressbook
-
-	$scope.editEntry = function (entry) {
-		$scope.form = angular.copy(entry);
-		$scope.edit = entry;
-	};
-
-// Address as used in the functions below refers to the contact methods (phone, email) of an entry
+// Add New Contact to Form
 
 	$scope.addContactValue= function () {
 		$scope.form.contactInfos.push(angular.copy(blankForm.contactInfos[0]));
 	};
 
+ /*************
+ * Edit Entry *
+ *************/
+
+// editEntry sets up the form for editing an entry but does not save changes
+	
+	$scope.editEntry = function (entry) {
+		$scope.form = angular.copy(entry);
+		$scope.form.removedContactInfos = [];
+		$scope.edit = true;
+	};
+
+// updateEntry actually updates the entry in the addressbook and resets the form
+ 
+// NOTE: THIS IS CURRENTLY NOT IMPLEMENTED IN THE API
+
+	$scope.updateEntry = function () {
+		Entry
+			.upsert($scope.form)
+			.$promise
+			.then(function (updatedEntry) {
+				$scope.entries.forEach(function (entry) {
+					if (entry.id === updatedEntry.id) {
+						var i = $scope.entries.indexOf(entry);
+						$scope.entries[i] = updatedEntry;
+					};
+				});
+				$scope.clearForm();
+			});
+	};
+
+ /*********
+ * Delete *
+ **********/
+
+// Delete Entry
+
+	$scope.removeEntry = function (entry) {
+		Entry
+			.deleteById(entry)
+			.$promise
+			.then(function () {
+				var i = $scope.entries.indexOf(entry);
+				$scope.entries.splice(i, 1);
+			});
+	};
+
+// Delete Contact Info From Form 
+
 	$scope.removeContactValue = function (contact) {
 		var i = $scope.form.contactInfos.indexOf(contact);
+		if ($scope.form.contactInfos[i].id) {
+			$scope.form.removedContactInfos.push($scope.form.contactInfos[i]);
+		};
 		$scope.form.contactInfos.splice(i, 1);
 	};
 
